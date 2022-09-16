a79ea57a0f9284333b39fd154818523a4d3c2c0d
Ratchanon Buachum <49112859+N9-1@users.noreply.github.com>
2022-09-17 01:18:17 +0700

    Update 7-10_print_pattren.py

--- KMUTNB/MIDTERM/7-10_print_pattren.py
+++ KMUTNB/MIDTERM/7-10_print_pattren.py
@@ -1,1 +1,44 @@
-while 
+n = 6
+# 7)
+print('7)')
+for i in range(n):
+    for j in range(i+1):
+        print(j+1, end='|')
+    print()
+
+print('-'*20)
+print('8)')
+# 8)
+for i in range(n):
+    for j in range(n-i):
+        print(j+1, end='|')
+    print()
+
+print('-'*20)
+print('9)')
+# 9)
+for i in range(n):
+    print(' '*((n-(i+1))*2), end='')
+    for j in range(i+1):
+        print(j+1, end='|')
+    print()
+
+print('-'*20)
+print('10)')
+for i in range(n, 0, -1):
+    print(' '*((n-i)*2), end='')
+    for j in range(i):
+        print(j+1, end='|')
+    print()
+
+print('-'*20)
+print('c) จากหนังสือ')
+print('reverse ตัวเลข')
+# C
+for i in range(n):
+    for j in range(n, 0, -1):
+        if j-1 > i:
+            print(" ", end=' ')
+        else:
+            print(j, end=' ')
+    print()
